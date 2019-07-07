913299c1fd364172ae2bed8493321b86ca5a8eec
rajitha302 <rajithadisanayaka@gmail.com>
2019-07-07 22:04:29 +0530

    new update

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -4,3 +4,7 @@ $(".tab").click(function(){
     $(this).addClass("tab-active");
     $(".content-" + this.id).show();
 })
+
+$(document).ready(function(){
+    alert('My portfolio is still under development! 🔨, please bear with me guys!😬');
+  });
